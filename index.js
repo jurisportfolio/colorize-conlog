18646b951b55748455accad82acdc90ccac52997
jurkotroll <jurko.bossetroll@gmail.com>
2019-05-21 19:28:20 +0200

    refuctoring part 1: divideLog, typeVerification

--- index.js
+++ index.js
@@ -1,57 +1,42 @@
 const prePreObj = { foo: "some", boo: 5, woo: [], too: null };
-
 const preTab = [1, 2, "napis", prePreObj];
-
 const preObj = { foo: "napis", boo: 3, woo: null, too: preTab };
-
 const tab = [1, "napis", preTab, preObj];
 
+
 const colorOf = (key, val) => {
-    let color = null;
-    switch (typeof val) {
-        case "number":
-            color = "blue";
-            break;
-        case "string":
-            color = "red";
-            break;
-        case "object":
-            if (val == null) {
-                color = "lightseagreen";
-                break;
-            } else if (Array.isArray(val)) {
-                color = "magenta";
-                logIfObjectOrArray("Array", val, color, key);
-                color = null;
-                break;
-            } else {
-                color = "green";
-                logIfObjectOrArray("Object", val, color, key);
-                color = null;
-                break;
-            }
-        default:
-            color = "black";
-    }
-    return color;
+  let color = null;
+  switch (typeof val) {
+    case "number":
+      color = "blue";
+      break;
+    case "string":
+      color = "red";
+      break;
+    case "object":
+      if (val == null) {
+        color = "lightseagreen";
+        break;
+      } else if (Array.isArray(val)) {
+        color = "magenta";
+        logIfCollection("Array", val, color, key);
+        color = null;
+        break;
+      } else {
+        color = "green";
+        logIfCollection("Object", val, color, key);
+        color = null;
+        break;
+      }
+    default:
+      color = "black";
+  }
+  return color;
 };
 
-const logIfNotObjectOrIsNull = (label, obj, isObjNull, typeOfObj) => {
-        return console.log(
-                `%c%s`,
-                "color: red",
-                `${label} : You try to log ${
-    isObjNull ? `"${obj}"` : typeOfObj
-    }. Function "prettyDir" accepts just iterable collections!`
-  );
-};
 
-// const logIfObjectIsEmpty = (label, obj) => {
-//   console.log(`%c --> ${label} : You try to log empty object:`, "color: red");
-//   console.dir(obj);
-// };
 
-const logIfObjectOrArray = (label, obj, color = null, key = null) => {
+const logIfCollection = (label, obj, color = null, key = null) => {
   label === "Array" || label === "Object"
     ? console.groupCollapsed(
       `%c%s`,
@@ -71,39 +56,54 @@ const logIfObjectOrArray = (label, obj, color = null, key = null) => {
   console.groupEnd(label);
 };
 
-const prettyDir = (label, obj) => {
-  let typeOfObj = typeof obj;
-  let isObjNull = obj == null;
+const divideLog = (message, position = 'END') => {
   console.log(
     `%c%s`,
-    "color: green; background: yellow;",
-    `START log --> ${label} `
+    `${position == 'END' ? "color: green; background: yellow;" : "color: mediumblue; background: moccasin;"}`,
+    `${position} log --> ${message} `
   );
-  const labelUpper = label.toUpperCase();
-  if (typeOfObj !== "object" || isObjNull) {
-    logIfNotObjectOrIsNull(labelUpper, obj, isObjNull, typeOfObj);
-  } else if (Object.values(obj).length <= 0) {
-    console.log(
-      "%c%s",
-      "color: red",
-      `${labelUpper} : You try to log empty object:`
-    );
-    logIfObjectOrArray(labelUpper, obj);
-  } else {
-    logIfObjectOrArray(labelUpper, obj);
-  }
-  console.log(
-    "%c%s",
-    "color: mediumblue; background: moccasin;",
-    `END log <-- ${label} `
-  );
+}
+
+
+const logIfNotCollection = (isPropNull, propType) => {
+  return console.log(
+    `%c%s`,
+    "color: red",
+    `You try to log "${isPropNull ? "null" : propType}". Function "prettyDir" accepts just iterable collections!`
+  );;
 };
 
-console.clear();
-// prettyDir("new1", preTab);
+const typeVerification = (prop) => {
+  let propType = typeof prop;
+  let isPropNull = prop == null;
+
+  if (propType !== "object" || isPropNull) {
+    logIfNotCollection(isPropNull, propType);
+    return false;
+  } else {
+    return true
+  }
+}
+
+const prettyDir = (label, obj) => {
+
+  const labelUpper = label.toUpperCase();
+
+  divideLog(label, 'START');
+
+  if (typeVerification(obj)) {
+    logIfCollection(labelUpper, obj);
+  }
+
+  divideLog(label, 'END');
+};
+
+
+prettyDir("new1", preTab);
 prettyDir("new2", tab);
-// prettyDir("new3", null);
-// prettyDir("new4", []);
-// prettyDir("new5", preObj);
-// prettyDir("", 6);
-// prettyDir("Array", tab);
+prettyDir("new3", null);
+prettyDir("new4", []);
+prettyDir("new5", preObj);
+prettyDir("", 6);
+prettyDir("new6", "new log");
+prettyDir("Array", tab);
